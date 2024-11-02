a872a944683cf59509a4c558900803ade0a74b58
Ashwani <ashwanikaushik133@gmail.com>
2024-11-02 13:31:39 +0530

    update filename

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import { Adopter } from './components/Adopter'
-import { FormInputs } from './components/FormInputs'
+import {FormInputs} from './components/FormInputs'
 
 function App() {
   const [showPetAdoptionForm, setShowPetAdoptionForm] = useState(false);
